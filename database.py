d6cb5fd684b010ace2bdb3bff58cd1bab612eada
Izcoatl Avila Marcos <43587292+QuietVenom@users.noreply.github.com>
2023-05-15 18:59:27 -0600

    database elephantsql

--- database.py
+++ database.py
@@ -2,13 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:virgia@127.0.0.1:3306/todoapp"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:virgia@localhost:5433/TodoAppDatabase"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:virgia@127.0.0.1:3306/todoapp"
+SQLALCHEMY_DATABASE_URL = "postgresql://gmlnfzhu:FJpxZv4ZxENGiKgR0JgbtLOLdg56S8gQ@mahmud.db.elephantsql.com/gmlnfzhu"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL #[sqlite], connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL  # [sqlite], connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind= engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
